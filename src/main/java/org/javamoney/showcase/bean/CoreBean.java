362255a3ef3f2ac046b3a69d65b4f9ab1e5d10f2
unknown <gshenoy@N5102INh9yld02.qlogic.org>
2014-06-23 15:15:31 +0530

    Added some tests for core api

--- src/main/java/org/javamoney/showcase/bean/CoreBean.java
+++ src/main/java/org/javamoney/showcase/bean/CoreBean.java
@@ -17,6 +17,8 @@ package org.javamoney.showcase.bean;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.money.CurrencyUnit;
+import org.javamoney.showcase.utiity.CurrencyUtil;
 
 /**
  *
@@ -25,35 +27,113 @@ import javax.faces.bean.SessionScoped;
 @ManagedBean
 @SessionScoped
 public class CoreBean {
-    
+
     private String functionality = "0";
-    
+    private String result = "";
+
+    private String currencyCode1 = "";
+
+    private String currencyCode2 = "";
+
     public CoreBean() {
-        
+
     }
-    
+
     public String getFunctionality() {
         return functionality;
     }
-    
+
     public void setFunctionality(String functionality) {
         this.functionality = functionality;
     }
-    
+
     public void test() {
         System.out.println("test()");
+
+        int fun = Integer.parseInt(getFunctionality());
+        CurrencyUnit cu;
+        switch (fun) {
+
+            case 0:
+
+                cu = CurrencyUtil.get().createCurrencyUnit(getCurrencyCode1());
+
+                if (cu != null) {
+
+                    result = formatResult(cu);
+
+                }
+
+                break;
+
+            case 1:
+
+                boolean equal = CurrencyUtil.get().equalsCurrencyUnits(getCurrencyCode1(), getCurrencyCode2());
+
+                result = "The currency units are " + (equal ? "Equal" : "Not equal");
+
+                break;
+
+            case 2:
+                cu = CurrencyUtil.get().createCurrencyUnit(getCurrencyCode1());
+                result = "Currency unit= " + cu.getCurrencyCode();
+                break;
+
+            case 3:
+                cu = CurrencyUtil.get().createCurrencyUnit(getCurrencyCode1());
+                result = "Numeric code= " + cu.getNumericCode();
+                break;
+
+            case 4:
+                cu = CurrencyUtil.get().createCurrencyUnit(getCurrencyCode1());
+                result = "Default Fractional digits= " + cu.getDefaultFractionDigits();
+                break;
+
+        }
+
     }
-    
+
     public void cancel() {
-        System.out.println("cancel()");
+        result = "";
     }
-    
+
     public boolean canShowCurrencyCode1() {
         return true;
     }
-    
+
     public boolean canShowCurrencyCode2() {
         return getFunctionality().equals("1");
     }
-    
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public String getCurrencyCode1() {
+        return currencyCode1;
+    }
+
+    public void setCurrencyCode1(String currencyCode1) {
+        this.currencyCode1 = currencyCode1;
+    }
+
+    public String getCurrencyCode2() {
+        return currencyCode2;
+    }
+
+    public void setCurrencyCode2(String currencyCode2) {
+        this.currencyCode2 = currencyCode2;
+    }
+
+    private String formatResult(CurrencyUnit cu) {
+
+        return "Currency Code= " + cu.getCurrencyCode() + " Fractional digits= " + cu.getDefaultFractionDigits()
+                + " Numeric code= " + cu.getNumericCode();
+
+    }
+
 }
